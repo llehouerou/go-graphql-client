llehouerou/go-graphql-client

commit e2d109631b30ec81ee9dcc84f05040eb218008ae

change variables type to interface{} so any struct can be used to define variables. map[string]interface{} is still working as before.

query.go
@@ -46,7 +46,7 @@ func constructOptions(options []Option) (*constructOptionsOutput, error) {
 }
 
 // ConstructQuery build GraphQL query string from struct and variables
-func ConstructQuery(v interface{}, variables map[string]interface{}, options ...Option) (string, error) {
+func ConstructQuery(v interface{}, variables interface{}, options ...Option) (string, error) {
 	query, err := query(v)
 	if err != nil {
 		return "", err
@@ -57,8 +57,11 @@ func ConstructQuery(v interface{}, variables map[string]interface{}, options ...
 		return "", err
 	}
 
-	if len(variables) > 0 {
-		return fmt.Sprintf("query %s(%s)%s%s", optionsOutput.operationName, queryArguments(variables), optionsOutput.OperationDirectivesString(), query), nil
+	if variables != nil {
+		reflectVal := reflect.ValueOf(variables)
+		if reflectVal.Kind() != reflect.Map || (reflectVal.Kind() == reflect.Map && reflectVal.Len() > 0) {
+			return fmt.Sprintf("query %s(%s)%s%s", optionsOutput.operationName, queryArguments(variables), optionsOutput.OperationDirectivesString(), query), nil
+		}
 	}
 
 	if optionsOutput.operationName == "" && len(optionsOutput.operationDirectives) == 0 {
@@ -69,7 +72,7 @@ func ConstructQuery(v interface{}, variables map[string]interface{}, options ...
 }
 
 // ConstructQuery build GraphQL mutation string from struct and variables
-func ConstructMutation(v interface{}, variables map[string]interface{}, options ...Option) (string, error) {
+func ConstructMutation(v interface{}, variables interface{}, options ...Option) (string, error) {
 	query, err := query(v)
 	if err != nil {
 		return "", err
@@ -78,8 +81,11 @@ func ConstructMutation(v interface{}, variables map[string]interface{}, options 
 	if err != nil {
 		return "", err
 	}
-	if len(variables) > 0 {
-		return fmt.Sprintf("mutation %s(%s)%s%s", optionsOutput.operationName, queryArguments(variables), optionsOutput.OperationDirectivesString(), query), nil
+	if variables != nil {
+		reflectVal := reflect.ValueOf(variables)
+		if reflectVal.Kind() != reflect.Map || (reflectVal.Kind() == reflect.Map && reflectVal.Len() > 0) {
+			return fmt.Sprintf("mutation %s(%s)%s%s", optionsOutput.operationName, queryArguments(variables), optionsOutput.OperationDirectivesString(), query), nil
+		}
 	}
 
 	if optionsOutput.operationName == "" && len(optionsOutput.operationDirectives) == 0 {
@@ -90,7 +96,7 @@ func ConstructMutation(v interface{}, variables map[string]interface{}, options 
 }
 
 // ConstructSubscription build GraphQL subscription string from struct and variables
-func ConstructSubscription(v interface{}, variables map[string]interface{}, options ...Option) (string, error) {
+func ConstructSubscription(v interface{}, variables interface{}, options ...Option) (string, error) {
 	query, err := query(v)
 	if err != nil {
 		return "", err
@@ -99,8 +105,11 @@ func ConstructSubscription(v interface{}, variables map[string]interface{}, opti
 	if err != nil {
 		return "", err
 	}
-	if len(variables) > 0 {
-		return fmt.Sprintf("subscription %s(%s)%s%s", optionsOutput.operationName, queryArguments(variables), optionsOutput.OperationDirectivesString(), query), nil
+	if variables != nil {
+		reflectVal := reflect.ValueOf(variables)
+		if reflectVal.Kind() != reflect.Map || (reflectVal.Kind() == reflect.Map && reflectVal.Len() > 0) {
+			return fmt.Sprintf("subscription %s(%s)%s%s", optionsOutput.operationName, queryArguments(variables), optionsOutput.OperationDirectivesString(), query), nil
+		}
 	}
 	if optionsOutput.operationName == "" && len(optionsOutput.operationDirectives) == 0 {
 		return "subscription" + query, nil
@@ -111,25 +120,61 @@ func ConstructSubscription(v interface{}, variables map[string]interface{}, opti
 // queryArguments constructs a minified arguments string for variables.
 //
 // E.g., map[string]interface{}{"a": int(123), "b": true} -> "$a:Int!$b:Boolean!".
-func queryArguments(variables map[string]interface{}) string {
-	// Sort keys in order to produce deterministic output for testing purposes.
-	// TODO: If tests can be made to work with non-deterministic output, then no need to sort.
-	keys := make([]string, 0, len(variables))
-	for k := range variables {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-
+func queryArguments(variables interface{}) string {
+	var keys []string
 	var buf bytes.Buffer
-	for _, k := range keys {
-		io.WriteString(&buf, "$")
-		io.WriteString(&buf, k)
-		io.WriteString(&buf, ":")
-		writeArgumentType(&buf, reflect.TypeOf(variables[k]), variables[k], true)
-		// Don't insert a comma here.
-		// Commas in GraphQL are insignificant, and we want minified output.
-		// See https://facebook.github.io/graphql/October2016/#sec-Insignificant-Commas.
+
+	switch v := variables.(type) {
+	case map[string]interface{}:
+		for k := range v {
+			keys = append(keys, k)
+		}
+		sort.Strings(keys)
+		for _, k := range keys {
+			io.WriteString(&buf, "$")
+			io.WriteString(&buf, k)
+			io.WriteString(&buf, ":")
+			writeArgumentType(&buf, reflect.TypeOf(v[k]), v[k], true)
+		}
+	default:
+		val := reflect.ValueOf(variables)
+		typ := reflect.TypeOf(variables)
+		if typ.Kind() == reflect.Ptr {
+			val = val.Elem()
+			typ = typ.Elem()
+		}
+		if typ.Kind() != reflect.Struct {
+			panic(fmt.Sprintf("variables must be a struct or a map; got %T", variables))
+		}
+		for i := 0; i < val.NumField(); i++ {
+			field := typ.Field(i)
+			jsonTag := field.Tag.Get("json")
+			if jsonTag == "" {
+				continue
+			}
+			jsonName := jsonTag
+			if commaIdx := bytes.IndexByte([]byte(jsonTag), ','); commaIdx > -1 {
+				jsonName = jsonTag[:commaIdx]
+			}
+			if field.PkgPath != "" {
+				continue
+			}
+			keys = append(keys, jsonName)
+		}
+		sort.Strings(keys)
+		for _, jsonName := range keys {
+			field, _ := typ.FieldByNameFunc(func(s string) bool {
+				field, _ := typ.FieldByName(s)
+				return field.Tag.Get("json") == jsonName
+			})
+			value := val.FieldByName(field.Name)
+			io.WriteString(&buf, "$")
+			io.WriteString(&buf, jsonName)
+			io.WriteString(&buf, ":")
+			writeArgumentType(&buf, field.Type, value.Interface(), true)
+		}
 	}
+
 	return buf.String()
 }
 

graphql.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"reflect"
 	"strings"
 
 	"github.com/llehouerou/go-graphql-client/pkg/jsonutil"
@@ -42,61 +43,35 @@ func NewClient(url string, httpClient *http.Client) *Client {
 // Query executes a single GraphQL query request,
 // with a query derived from q, populating the response into it.
 // q should be a pointer to struct that corresponds to the GraphQL schema.
-func (c *Client) Query(ctx context.Context, q interface{}, variables map[string]interface{}, options ...Option) error {
+func (c *Client) Query(ctx context.Context, q interface{}, variables interface{}, options ...Option) error {
 	return c.do(ctx, queryOperation, q, variables, options...)
-}
-
-// NamedQuery executes a single GraphQL query request, with operation name
-//
-// Deprecated: this is the shortcut of Query method, with NewOperationName option
-func (c *Client) NamedQuery(ctx context.Context, name string, q interface{}, variables map[string]interface{}, options ...Option) error {
-	return c.do(ctx, queryOperation, q, variables, append(options, OperationName(name))...)
 }
 
 // Mutate executes a single GraphQL mutation request,
 // with a mutation derived from m, populating the response into it.
 // m should be a pointer to struct that corresponds to the GraphQL schema.
-func (c *Client) Mutate(ctx context.Context, m interface{}, variables map[string]interface{}, options ...Option) error {
+func (c *Client) Mutate(ctx context.Context, m interface{}, variables interface{}, options ...Option) error {
 	return c.do(ctx, mutationOperation, m, variables, options...)
-}
-
-// NamedMutate executes a single GraphQL mutation request, with operation name
-//
-// Deprecated: this is the shortcut of Mutate method, with NewOperationName option
-func (c *Client) NamedMutate(ctx context.Context, name string, m interface{}, variables map[string]interface{}, options ...Option) error {
-	return c.do(ctx, mutationOperation, m, variables, append(options, OperationName(name))...)
 }
 
 // Query executes a single GraphQL query request,
 // with a query derived from q, populating the response into it.
 // q should be a pointer to struct that corresponds to the GraphQL schema.
 // return raw bytes message.
-func (c *Client) QueryRaw(ctx context.Context, q interface{}, variables map[string]interface{}, options ...Option) ([]byte, error) {
+func (c *Client) QueryRaw(ctx context.Context, q interface{}, variables interface{}, options ...Option) ([]byte, error) {
 	return c.doRaw(ctx, queryOperation, q, variables, options...)
-}
-
-// NamedQueryRaw executes a single GraphQL query request, with operation name
-// return raw bytes message.
-func (c *Client) NamedQueryRaw(ctx context.Context, name string, q interface{}, variables map[string]interface{}, options ...Option) ([]byte, error) {
-	return c.doRaw(ctx, queryOperation, q, variables, append(options, OperationName(name))...)
 }
 
 // MutateRaw executes a single GraphQL mutation request,
 // with a mutation derived from m, populating the response into it.
 // m should be a pointer to struct that corresponds to the GraphQL schema.
 // return raw bytes message.
-func (c *Client) MutateRaw(ctx context.Context, m interface{}, variables map[string]interface{}, options ...Option) ([]byte, error) {
+func (c *Client) MutateRaw(ctx context.Context, m interface{}, variables interface{}, options ...Option) ([]byte, error) {
 	return c.doRaw(ctx, mutationOperation, m, variables, options...)
 }
 
-// NamedMutateRaw executes a single GraphQL mutation request, with operation name
-// return raw bytes message.
-func (c *Client) NamedMutateRaw(ctx context.Context, name string, m interface{}, variables map[string]interface{}, options ...Option) ([]byte, error) {
-	return c.doRaw(ctx, mutationOperation, m, variables, append(options, OperationName(name))...)
-}
-
 // buildAndRequest the common method that builds and send graphql request
-func (c *Client) buildAndRequest(ctx context.Context, op operationType, v interface{}, variables map[string]interface{}, options ...Option) ([]byte, *http.Response, io.Reader, Errors) {
+func (c *Client) buildAndRequest(ctx context.Context, op operationType, v interface{}, variables interface{}, options ...Option) ([]byte, *http.Response, io.Reader, Errors) {
 	var query string
 	var err error
 	switch op {
@@ -114,10 +89,16 @@ func (c *Client) buildAndRequest(ctx context.Context, op operationType, v interf
 }
 
 // Request the common method that send graphql request
-func (c *Client) request(ctx context.Context, query string, variables map[string]interface{}, options ...Option) ([]byte, *http.Response, io.Reader, Errors) {
+func (c *Client) request(ctx context.Context, query string, variables interface{}, options ...Option) ([]byte, *http.Response, io.Reader, Errors) {
+	if variables != nil {
+		reflectVal := reflect.ValueOf(variables)
+		if reflectVal.Kind() == reflect.Map && reflectVal.Len() == 0 {
+			variables = nil
+		}
+	}
 	in := struct {
-		Query     string                 `json:"query"`
-		Variables map[string]interface{} `json:"variables,omitempty"`
+		Query     string      `json:"query"`
+		Variables interface{} `json:"variables,omitempty"`
 	}{
 		Query:     query,
 		Variables: variables,
@@ -212,7 +193,7 @@ func (c *Client) request(ctx context.Context, query string, variables map[string
 
 	var rawData []byte
 	if out.Data != nil && len(*out.Data) > 0 {
-		rawData = []byte(*out.Data)
+		rawData = *out.Data
 	}
 
 	if len(out.Errors) > 0 {
@@ -230,7 +211,7 @@ func (c *Client) request(ctx context.Context, query string, variables map[string
 
 // do executes a single GraphQL operation.
 // return raw message and error
-func (c *Client) doRaw(ctx context.Context, op operationType, v interface{}, variables map[string]interface{}, options ...Option) ([]byte, error) {
+func (c *Client) doRaw(ctx context.Context, op operationType, v interface{}, variables interface{}, options ...Option) ([]byte, error) {
 	data, _, _, err := c.buildAndRequest(ctx, op, v, variables, options...)
 	if len(err) > 0 {
 		return data, err
@@ -239,7 +220,7 @@ func (c *Client) doRaw(ctx context.Context, op operationType, v interface{}, var
 }
 
 // do executes a single GraphQL operation and unmarshal json.
-func (c *Client) do(ctx context.Context, op operationType, v interface{}, variables map[string]interface{}, options ...Option) error {
+func (c *Client) do(ctx context.Context, op operationType, v interface{}, variables interface{}, options ...Option) error {
 	data, resp, respBuf, errs := c.buildAndRequest(ctx, op, v, variables, options...)
 	return c.processResponse(v, data, resp, respBuf, errs)
 }

commit f5543906773ffd07c177f8893168a4ef9176d2ab

fix: Fix critical bugs in struct-based variables (queryArguments)
Fix two bugs in the struct variable handling introduced in e2d1096:

1. **CRITICAL: Fix panic with json tag options** (P0)
   - Previous code: Field lookup used FieldByNameFunc comparing full json
     tag to extracted field name, causing mismatch for tags with options
   - Example: json:"name,omitempty" would fail lookup (compared "name" to
     "name,omitempty"), returning zero value, causing panic on line 223
   - Impact: Any struct with json:",omitempty" or similar would PANIC
   - Fix: Single-pass algorithm that captures field info during iteration,
     eliminating broken lookup logic

2. **Fix json:"-" fields not properly skipped**
   - Previous code: Only checked for empty json tag, not for "-"
   - Standard Go convention: json:"-" means "skip this field"
   - Impact: Fields with json:"-" were incorrectly included as "$-:Type!"
   - Fix: Added explicit check for jsonTag == "-"

Additional improvements:
- Performance: Reduced O(n²) complexity to O(n log n) by eliminating
  nested field lookups in favor of single-pass collection + sort
- Added validation for empty field names after parsing (e.g., json:",omitempty")
- Reordered checks: Skip unexported fields first before processing tags
- Added comprehensive inline comments explaining each step

Tests added:
- struct with json:"-" tag (now correctly skipped)
- struct with json tag options: omitempty, string (no longer panics)
- struct with empty field name in tag (correctly skipped)
- struct with multiple json:"-" tags
- Updated existing test that documented the json:"-" bug

All tests pass. No regressions.

🤖 Generated with [Claude Code](https://claude.com/claude-code)

Co-Authored-By: Claude <noreply@anthropic.com>

query.go
@@ -195,32 +195,59 @@ func queryArguments(variables any) string {
 		if typ.Kind() != reflect.Struct {
 			panic(fmt.Sprintf("variables must be a struct or a map; got %T", variables))
 		}
+
+		// Collect field information in a single pass
+		type fieldInfo struct {
+			jsonName  string
+			fieldType reflect.Type
+			value     reflect.Value
+		}
+		var fields []fieldInfo
+
 		for i := 0; i < val.NumField(); i++ {
 			field := typ.Field(i)
-			jsonTag := field.Tag.Get("json")
-			if jsonTag == "" {
+
+			// Skip unexported fields
+			if field.PkgPath != "" {
 				continue
 			}
+
+			jsonTag := field.Tag.Get("json")
+
+			// Skip fields without json tag or with json:"-"
+			if jsonTag == "" || jsonTag == "-" {
+				continue
+			}
+
+			// Extract field name from json tag (before comma if present)
 			jsonName := jsonTag
 			if commaIdx := bytes.IndexByte([]byte(jsonTag), ','); commaIdx > -1 {
 				jsonName = jsonTag[:commaIdx]
 			}
-			if field.PkgPath != "" {
+
+			// Skip if field name is empty after extraction
+			if jsonName == "" || jsonName == "-" {
 				continue
 			}
-			keys = append(keys, jsonName)
-		}
-		sort.Strings(keys)
-		for _, jsonName := range keys {
-			field, _ := typ.FieldByNameFunc(func(s string) bool {
-				field, _ := typ.FieldByName(s)
-				return field.Tag.Get("json") == jsonName
+
+			fields = append(fields, fieldInfo{
+				jsonName:  jsonName,
+				fieldType: field.Type,
+				value:     val.Field(i),
 			})
-			value := val.FieldByName(field.Name)
+		}
+
+		// Sort fields by json name for deterministic output
+		sort.Slice(fields, func(i, j int) bool {
+			return fields[i].jsonName < fields[j].jsonName
+		})
+
+		// Build the arguments string
+		for _, f := range fields {
 			_, _ = io.WriteString(&buf, "$")
-			_, _ = io.WriteString(&buf, jsonName)
+			_, _ = io.WriteString(&buf, f.jsonName)
 			_, _ = io.WriteString(&buf, ":")
-			writeArgumentType(&buf, field.Type, value.Interface(), true)
+			writeArgumentType(&buf, f.fieldType, f.value.Interface(), true)
 		}
 	}
 

query_test.go
@@ -911,7 +911,7 @@ func TestQueryArguments_StructVariables(t *testing.T) {
 			want: "$active:Boolean!$name:String!",
 		},
 		{
-			name: "struct with json:- tag (treated as field named '-')",
+			name: "struct with json:- tag (should skip)",
 			in: struct {
 				Name     string `json:"name"`
 				Internal string `json:"-"`
@@ -921,8 +921,50 @@ func TestQueryArguments_StructVariables(t *testing.T) {
 				Internal: "secret",
 				Active:   true,
 			},
-			// Note: This is a bug - json:"-" should be skipped but isn't in e2d1096
-			want: "$-:String!$active:Boolean!$name:String!",
+			want: "$active:Boolean!$name:String!",
+		},
+		{
+			name: "struct with json tag options (should extract field name only)",
+			in: struct {
+				Name     string `json:"name,omitempty"`
+				Age      int    `json:"age,string"`
+				Active   bool   `json:"active,omitempty"`
+				Optional *int   `json:"optional,omitempty"`
+			}{
+				Name:     "John",
+				Age:      30,
+				Active:   true,
+				Optional: &iVal,
+			},
+			want: "$active:Boolean!$age:Int!$name:String!$optional:Int",
+		},
+		{
+			name: "struct with empty field name in tag (should skip)",
+			in: struct {
+				Name   string `json:"name"`
+				Empty  string `json:",omitempty"` // Empty field name
+				Active bool   `json:"active"`
+			}{
+				Name:   "John",
+				Empty:  "ignored",
+				Active: true,
+			},
+			want: "$active:Boolean!$name:String!",
+		},
+		{
+			name: "struct with multiple json:- tags",
+			in: struct {
+				Name      string `json:"name"`
+				Internal1 string `json:"-"`
+				Internal2 int    `json:"-"`
+				Active    bool   `json:"active"`
+			}{
+				Name:      "John",
+				Internal1: "secret",
+				Internal2: 42,
+				Active:    true,
+			},
+			want: "$active:Boolean!$name:String!",
 		},
 		{
 			name: "struct with simple fields only",